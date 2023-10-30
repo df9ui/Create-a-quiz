980c1329c60034456d37f15f4cee4076433d99b7
df9ui <145046261+df9ui@users.noreply.github.com>
2023-10-30 10:58:19 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,15 @@ function btnClicked() {
   let input2 = document.getElementById("input-2").value.toLowerCase();
   let input3 = document.getElementById("input-3").value;
   let input4 = document.getElementById("input-4").value;
+  //input values
+  
   let output1 = document.getElementById("output-1");
   let output2 = document.getElementById("output-2");
   let output3 = document.getElementById("output-3");
   let output4 = document.getElementById("output-4");
+  //output values
+
+  //question 1 verify
   if (input1 === "plant cell") {
     output1.style.color = "green";
     output1.innerHTML = "Correct";
@@ -17,6 +22,8 @@ function btnClicked() {
     output1.style.color = "red";
     output1.innerHTML = "Incorrect";
   }
+
+  //question 2 verify
   if (input2 === "mitochondria") {
     output2.style.color = "green";
     output2.innerHTML = "Correct";
@@ -25,6 +32,8 @@ function btnClicked() {
     output2.style.color = "red";
     output2.innerHTML = "Incorrect";
   }
+
+  //question 3 verify
   if (input3 === "1") {
     output3.style.color = "green";
     output3.innerHTML = "Correct";
@@ -33,6 +42,8 @@ function btnClicked() {
     output3.style.color = "red";
     output3.innerHTML = "Incorrect";
   }
+
+  //question 4 verify
   if (input4 === "Mr.Hogan" || input4 === "Mr.H") {
     output4.style.color = "green";
     output4.innerHTML = "Correct";
@@ -41,6 +52,8 @@ function btnClicked() {
     output4.style.color = "red";
     output4.innerHTML = "Incorrect";
   }
+
+  //feedback
   document.getElementById("msg").innerHTML = num;
   if (num > 50) {
     document.getElementById("comment").innerHTML = "Great job! ";
